c8b0450c6e6347b99f138a5add6159ae3db5ade9
CH624 <zczc1010@naver.com>
2020-03-11 20:00:16 +0900

    회원가입 수정

--- src/main/java/com/insutil/ch/member/service/MemberService.java
+++ src/main/java/com/insutil/ch/member/service/MemberService.java
@@ -6,10 +6,6 @@ import com.insutil.ch.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
